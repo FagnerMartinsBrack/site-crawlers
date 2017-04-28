6c5d3f417d0811515b2b1bce8fb7a0991e4f4168
Fagner Brack <github@fagnermartins.com>
2017-04-28 20:13:08 +1000

    Allow passing a filter to the debit transactions

--- CommonWealth NetBank/Transactions.js
+++ CommonWealth NetBank/Transactions.js
@@ -1,4 +1,4 @@
-// Example: getDebitFrom('WOOLWORTHS')
+// Example: getDebitFrom('WOOLWORTHS', (value) => value > -10)
 
 function isPositive(number) {
   return number > 0;
@@ -10,10 +10,17 @@ function findElement(context, selector) {
   return element;
 }
 
-function getDebitFrom(merchantInput) {
+function findValueFor(transaction) {
+  const innerText = findElement(transaction, '.currencyUI').innerText
+  const textWithoutDollarSign = innerText.split('$').join('');
+  const textWithoutComma = textWithoutDollarSign.split(',').join('');
+  return Number(textWithoutComma)
+}
+
+function getDebitFrom(merchantInput, filter) {
   const table = document.getElementById('transactionsTableBody');
   const transactions = table.querySelectorAll('tr');
-  console.log(transactions.length, 'transactions')
+  console.info(transactions.length + ' transactions')
 
   const filteredTransactions = [].slice.call(transactions).filter(function(transaction) {
     try {
@@ -22,17 +29,24 @@ function getDebitFrom(merchantInput) {
       if (matchesMerchant) return true;
     } catch (e) {}
   });
-  console.log(filteredTransactions.length, 'transactions for: ', merchantInput);
+  console.info(filteredTransactions.length + ' transactions for: ' + merchantInput);
 
-  return filteredTransactions.reduce(function(previous, current) {
-    const innerText = findElement(current, '.currencyUI').innerText
-    const textWithoutDollarSign = innerText.split('$').join('');
-    const textWithoutComma = textWithoutDollarSign.split(',').join('');
-    const asNumber = Number(textWithoutComma)
+  const debitTransactions = filteredTransactions.filter(function(transaction) {
+    const value = findValueFor(transaction);
     // Is debit transaction?
-    if (!isPositive(asNumber)) {
-      console.log('text: ' + innerText, 'asNumber: ' + Number(asNumber));
-      return previous + asNumber
+    if (!isPositive(value)) {
+      return true;
+    }
+    return false;
+  });
+  console.info(debitTransactions.length + ' debit transactions for: ' + merchantInput);
+
+  return debitTransactions.reduce(function(previous, currentTransaction) {
+    const value = findValueFor(currentTransaction);
+    if (filter(value)) {
+      const innerText = findElement(currentTransaction, '.currencyUI').innerText
+      console.log('text: ' + innerText, 'asNumber: ' + value);
+      return previous + value
     }
     return previous;
   }, 0);
